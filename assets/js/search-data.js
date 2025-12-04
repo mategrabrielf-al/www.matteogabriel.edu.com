739e9c89af318fa6fa094b23fceadbe576eb881a
mategrabrielf-al <sotoinsaurraldef@gmail.com>
2025-12-04 13:42:22 +0000

    Deploying to gh-pages from @ mategrabrielf-al/www.matteogabriel.edu.com@7641df7730e7ae46c361e63bf1b3f4fd46a685f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -463,9 +463,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/www.matteogabriel.edu.com/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/www.matteogabriel.edu.com/projects/3_project/";
             },},{id: "projects-project-4",
